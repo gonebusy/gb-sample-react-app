e781a92e40d690daf5ee2845d60fb32d479b4799
Joseph Lim <josephlim@Josephs-MBP.fios-router.home>
2017-06-15 01:22:14 -0400

    have app fetch booking information

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Routes from 'src/js/routes';
-import { fetchStaff } from 'src/js/actions/staff';
+import { fetchStaff, fetchBookings } from 'src/js/actions/staff';
 import store from '../store';
 import '../../scss/main.scss';
 
 class App extends Component {
     componentWillMount() {
         store.dispatch(fetchStaff());
+        store.dispatch(fetchBookings());
     }
 
     render() {
